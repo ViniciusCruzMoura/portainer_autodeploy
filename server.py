8e94e1b1a08de64e30215501820be9fe830bfef4
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-01-12 13:53:51 -0400

    update response

--- server.py
+++ server.py
@@ -9,10 +9,6 @@ app = flask.Flask(__name__)
 
 @app.route("/portainer/deploy/webhook/<action>/<stack_name>", methods = ['POST'])
 def webhook(action, stack_name):
-    if action is None or action == "":
-        return flask.jsonify({"mensagem": "FALHA", "detalhe": None})
-    if stack_name is None or stack_name == "":
-        return flask.jsonify({"mensagem": "FALHA", "detalhe": None})
     if portainer.main(["portainer.py", action, stack_name]) == 1:
-        return flask.jsonify({"mensagem": "FALHA", "detalhe": None})
-    return flask.jsonify({"mensagem": "SUCESSO", "detalhe": None})
+        return flask.Response(status=500)
+    return flask.Response(status=200)
