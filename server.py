98a0975260de0939f667e5bee96be23133909633
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-01-12 13:38:29 -0400

    add comment

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import flask
 import portainer
 
+# Start webhook server
+# command:
+#   flask --app server run --host=0.0.0.0:5000
+
 app = flask.Flask(__name__)
 
 @app.route("/portainer/deploy/webhook/<action>/<stack_name>", methods = ['POST'])
-def hello_world(action, stack_name):
+def webhook(action, stack_name):
     if action is None or action == "":
         return flask.jsonify({"mensagem": "FALHA", "detalhe": None})
     if stack_name is None or stack_name == "":
@@ -12,3 +16,4 @@ def hello_world(action, stack_name):
     if portainer.main(["portainer.py", action, stack_name]) == 1:
         return flask.jsonify({"mensagem": "FALHA", "detalhe": None})
     return flask.jsonify({"mensagem": "SUCESSO", "detalhe": None})
+
